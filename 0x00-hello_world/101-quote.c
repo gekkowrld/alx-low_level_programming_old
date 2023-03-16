52d484801969a5c3f2bf985c398323691bbbcad7
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-16 21:30:40 +0000

    Add relevant function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 */
 int main(void)
 {
-	printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (0);
 }
