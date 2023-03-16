ae5ca41cc3bf999d4d8bf88cbdf74317dee41d3b
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-16 21:33:45 +0000

    Add a parameter

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 /**
 * main - prints to the stderr
 *
-* Return: 0
+* Return: 1
 */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	return (0);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
 }
