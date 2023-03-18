96caaf99172a8e70ba9e277e965dccefda5925ab
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-18 09:11:56 +0000

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+/**
+* main - prints numbers 0- 9 using putchar
+* Return: 0
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar('0' + x);
+	putchar('\n');
+
+	return (0);
+}
