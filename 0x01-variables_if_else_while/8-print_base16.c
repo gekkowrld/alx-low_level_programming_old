29a3a12e844e98c3e90e8336290fe888b444b355
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-18 10:29:28 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+/**
+* main - prints hexadecimal numbers
+* Return: 0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 16; x++)
+	{
+		if (x < 10);
+			putchar('0' + x);
+		else
+			putchar(97 + x);
+	}
+	putchar('\n');
+
+	return (0);
+}
