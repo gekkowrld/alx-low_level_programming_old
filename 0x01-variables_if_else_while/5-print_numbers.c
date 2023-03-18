3f6edb880f8d40de7dfb12596c9efb82e09b9aae
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-18 08:52:32 +0000

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+/**
+* main - print numbers of base 10
+* Return: 0
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}
