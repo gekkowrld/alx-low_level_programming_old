bf202511fcd5c7c509dbfdc24b09bfdaff1882b8
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-18 09:17:45 +0000

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+/**
+* main - prints alphabets in reverse
+* Return: 0
+*/
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; i > 'a'; i--)
+		putchar(x);
+	putchar('\n');
+
+	return (0);
+}
