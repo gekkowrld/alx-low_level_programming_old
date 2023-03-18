a95c6d4af19ed01945d9028735dc083b545be3ea
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-18 08:27:11 +0000

    typo fix:

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,9 +8,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 'a'; i < 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 		putchar(i);
-	for (i = 'A'; i < 'Z'; i++)
+	for (i = 'A'; i <= 'Z'; i++)
 		putchar(i);
 	putchar('\n');
 
