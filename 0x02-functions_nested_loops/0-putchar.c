d8762df3c212013387b388915828ba0b5d4cebd9
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-21 21:59:28 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+/**
+* main - print _putchar
+* Return: 0
+*/
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar('\n');
+
+	return (0);
+}
