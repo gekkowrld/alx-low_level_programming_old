27290e45472c9fef65c3ab19737a2492fa4921d5
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-23 09:59:07 +0300

    Edit the header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,20 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+int positive_or_negative(int i);
 int largest_number(int, int, int);
-int print_remaining_days(int, int, int);
+int print_remaining_days(int month, int day, int year);
 
 #endif
