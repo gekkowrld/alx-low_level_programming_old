7056f4b3247e0f02e0ad06b06e403938c819d02c
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-23 20:11:46 +0300

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+# include "main.h"
+
+/**
+* print_numbers - prints numbers 0 - 9
+* and a new line
+* Return: void
+*/
+void print_numbers(void)
+{
+	int i;
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+	_putchar('\n');
+}
