35c808a35c266547a0126cb397566ab1de05b4ce
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-25 01:00:54 +0300

    Edit the main file fixing the typo

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,7 @@
 
 int positive_or_negative(int i);
 int largest_number(int, int, int);
-int print_remaining_days(int month, int day, int year);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif
