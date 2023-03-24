3a2f3623eca646d322937eda53a326e368c7e2a9
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-25 00:51:03 +0300

    Prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,22 @@
+# include <stdio.h>
+
+/**
+* main - print the largest prime number
+* Return: 0
+*/
+int main(void)
+{
+	long int j,k;
+
+	j = 612852475143;
+	for (k = 2; k <= j; k++)
+	{
+		if (j % k == 0)
+		{
+			j /=k;
+			k--;
+		}
+	}
+	printf("%ld\n", k);
+	return (0);
+}
