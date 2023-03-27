8bc40b55cc20986c6138673f248441cc57f780a7
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-28 00:52:24 +0300

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+# include "main.h"
+# include <stdio.h>
+
+/**
+* _puts - prints a string with a trailing newline
+* @str: the string to be printed
+* Return: string
+*/
+void _puts(char *str)
+{
+	return (puts(str));
+}
