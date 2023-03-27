c1472cb1d2fbe3ac893249c1207f5f9fc7574eb5
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-28 01:19:02 +0300

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+# include "main.h"
+# include <string.h>
+# include <stdio.h>
+/**
+* print_rev - prints text in reverse order
+* @s: the string to be reversed
+*/
+
+void print_rev(char *s)
+{
+	s = "This is a long string of text";
+	int i;
+	char temp;
+	int k = strlen(s);
+	int l = k - 1;
+
+	for (i = 0; i < l; i++)
+	{
+		temp = s[i];
+		s[i] = s[l];
+		s[l] = temp;
+		l--;
+	}
+	puts(s);
+}
