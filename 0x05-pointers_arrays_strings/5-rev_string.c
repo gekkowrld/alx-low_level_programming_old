0e723b959a8239d5812bb0668974a82658bc1a10
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-28 01:54:26 +0300

    Will it work?

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+# include "main.h"
+
+/**
+* rev_string - prints text in reverse order
+* @s: the string to be reversed
+*/
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (s[i] == '\0')
+			break;
+		i++;
+	}
+	for (i--; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
 int _putchar(char);
+void rev_string(char *s);
 
 #endif
