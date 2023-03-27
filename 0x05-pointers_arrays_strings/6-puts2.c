ee03e83bf53e519626b2c35ea9f5cd0b7c7ba76b
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-28 02:10:27 +0300

    Half of the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+# include "main.h"
+
+/**
+* puts2 - prints every other number
+* @str: the string that contains the characters to be printed
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -8,5 +8,6 @@ void _puts(char *str);
 void print_rev(char *s);
 int _putchar(char);
 void rev_string(char *s);
+void puts2(char * str);
 
 #endif
