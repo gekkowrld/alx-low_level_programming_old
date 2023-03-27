51e047b30f2c0ff765ebfaf5f3f6c26b05c1e7cd
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-28 02:02:43 +0300

    Use the right tools for the job

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,16 +7,24 @@
 
 void rev_string(char *s)
 {
-	int i = 0;
+	int count = 0, i, j;
+	char *str, temp;
 
-	_puts(s);
-	while (i >= 0)
+	while (count >= 0)
 	{
-		if (s[i] == '\0')
+		if (s[count] == '\0')
 			break;
-		i++;
+		count++;
 	}
-	for (i--; i >= 0; i--)
-		_putchar(s[i]);
-	_putchar('\n');
+	str = s;
+
+	for (i = 0; i < (count - 1); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
+	}
 }
