4eae72002edd3356895236014f221d2640c625a9
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-28 00:38:45 +0300

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+# include "main.h"
+# include <stdio.h>
+# include <string.h>
+
+/**
+* _strlen - prints out the lenght of a string
+* @s: the string to be measured
+* Return: lenght of the string
+*/
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define _MAIN_H_
 
 void reset_to_98(int *n);
-void swap_int(int *a, int *b)
+void swap_int(int *a, int *b);
+void _strlen(char *s);
 
 #endif
