0b374eab0562e5f78c38e83fe5de5e7092b7ea0a
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-03-29 02:24:04 +0300

    Make it come close to the magic number

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,5 @@
 # include <stdio.h>
 # include <stdlib.h>
-# include <string.h>
 # include <time.h>
 # include <unistd.h>
 
@@ -11,35 +10,20 @@
 
 int main(void)
 {
-	srand((time(0) * getpid()) + (time(0) % getpid()) - getpid());
+	int i, j = 0, k = 0, s = 32, e = 126;
 
-	int l = (rand() % 64), i;
-	char *passwd = malloc(l + 1);
-	char *d = "0123456789";
-	char *lc = "abcdefghijklmnopqrstuvwxyz";
-	char *uc =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int dl = strlen(d);
-	int lcl = strlen(lc);
-	char *s = "!@#$ *{}";
-	int sl = strlen(s);
+	srand((time(0) * getpid()) + getpid());
 
-
-	for (i = 0; i < l; i++)
+	while (j < 2772)
 	{
-		int ct = rand() % 4;
-
-		if (ct == 0)
-			passwd[i] = d[rand() % dl];
-		else if (ct == 1)
-			passwd[i] = lc[rand() % lcl];
-		else if (ct == 2)
-			passwd[i] = uc[rand() % lcl];
-		else
-			passwd[i] = s[rand() % sl];
+		k = rand() % 128;
+		if (k >= s && k <= e)
+		{
+			if ((j - k) > 2772)
+				break;
+			j += k;
+		printf("%c", k);
+		}
 	}
-	passwd[l] = '\0';
-	printf("%s\n", passwd);
-	free(passwd);
-
-	return (0);
+	putchar('\n');
 }
