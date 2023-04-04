3a0e23b3e7fc25d2f0c2a7bf1c9af09cfa7f913b
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-04 19:54:23 +0300

    Add a type cast function

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+# include "main.h"
+
+/**
+ * set_string - sets value of a pointer to a char
+ * @s: The src string
+ * @to: The reference string
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,5 +10,6 @@ void print_chessboard(char (*a)[8]);
 char *_strstr(char *haystack, char *needle);
 int _putchar(char c);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
