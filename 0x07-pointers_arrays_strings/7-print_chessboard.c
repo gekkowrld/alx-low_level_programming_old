a3a99ce32204e6d37f95709b93b67f28898dbcd0
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-04 03:29:36 +0300

    Buld a chess board using just charcters

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+# include "main.h"
+
+/**
+ * print_chessboard - prints the 8 by 8 chessboard
+ * @a: The characters to be printed out
+*/
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+
+		}
+		printf("\n");
+	}
+}
+
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -6,5 +6,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
+char *_strstr(char *haystack, char *needle);
 
 #endif
