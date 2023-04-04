7a9876f014207fffcac52aa151aa258d9a8c8cd0
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-04 18:28:32 +0300

    Fix the calc function

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,12 +12,9 @@ void print_diagsums(int *a, int size)
 	int i, j = 0, k = 0;
 
 	for (i = 0; i < size; i++)
-	{
 		j += a[i * size + i];
-	}
-	for (i = size - 1; i >= 0; i--)
-	{
+	for (i = 0; i < size ; i++)
 		k += a[i * size + (size - i - 1)];
-	}
+
 	printf("%d, %d\n", j, k);
 }
