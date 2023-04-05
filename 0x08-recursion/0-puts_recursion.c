89b2200e8a5b99371d404d3a48318bfe74fa1bbb
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-05 22:05:18 +0300

    Add a custom implementation of puts

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+# include "main.h"
+/**
+* _puts_recursion - recursively prints out a string
+* @s: The character to be printed out recursively
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+}
