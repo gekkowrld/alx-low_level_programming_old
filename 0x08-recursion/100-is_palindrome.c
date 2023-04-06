81c0c4c110523030b7b639b9fe0594274577666d
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-07 02:46:24 +0300

    Add the strlen function for lenght counting

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,6 +1,21 @@
 # include "main.h"
 
 /**
+ * _strlen_recursion - counts the length of the string
+ *@s: The character to be counted
+ * Return: The lenght of the string
+*/
+
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
+}
+
+
+/**
  * palindrome - loops through a string to check the similarity
  * @s: The src string
  * @i: The first index
