8e6f2e580b99d7fd013a5ef5041efbf2d91c876c
Gordon <108436933+gekkowrld@users.noreply.github.com>
2023-04-08 23:40:55 +0300

    Add a description of the main function

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,13 @@
-# include "stdio.h"
-
+# include "main.h"
+/**
+* main - prints out the program name
+* @argc: The arg count
+* @argv: The arg variable
+* Return: 0
+*/
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
-	return 0;
+	return (0);
 }
