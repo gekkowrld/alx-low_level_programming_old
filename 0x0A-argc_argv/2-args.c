333578612e110a5abe95e7d56ff82ac256e94db9
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-09 19:18:10 +0300

    Print out each argument in its line

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+/**
+* main - prints out the arguments
+* @argc: The argument counter
+* @argv: The argument variable
+* Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+
+	return (0);
+}
