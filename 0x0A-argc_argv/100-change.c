478ff33473d44740ccc9c0feb46b3d6640427f35
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-10 22:47:53 +0300

    Can we get change please!

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+# include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	int n = 0;
+
+	if (argc == 2)
+	{
+		int k[] = {25, 10, 5, 2, 1};
+		int i, j = atoi(argv[1]);
+
+		for (i = 0; i < 5; i++)
+		{
+			while (j >= k[i])
+			{
+				j -= k[i];
+				n++;
+			}
+		}
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", n);
+}
