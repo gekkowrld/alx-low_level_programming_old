12c1bca5d9cc22246f5c2dde0d917f2c2e0cbc35
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-13 21:11:35 +0300

    Add a return value

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,11 @@
 # include "main.h"
 # include <stdlib.h>
+
 /**
 * malloc_checked - checks if error occurs during the
 * allocation of memory using malloc
 * @b: The value
-* Return: void
+* Return: ptr
 */
 
 void *malloc_checked(unsigned int b)
@@ -13,4 +14,6 @@ void *malloc_checked(unsigned int b)
 
 	if (ptr == NULL)
 		exit(98);
+
+	return (ptr);
 }
