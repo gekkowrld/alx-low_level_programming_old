c1f720f5911cae50af4d75ea6a4fc9b54d849f4b
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-17 21:54:37 +0300

    Add identifiers, to identify the identity

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,14 @@ void print_dog(struct dog *d)
 		return;
 
 	if (d->name == NULL)
-		printf("nil\n");
+		printf("Name: nil\n");
 	else
 		printf("Name: %s\n", d->name);
 
 	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("nil\n");
+		printf("Name: nil\n");
 	else
 		printf("Owner: %s\n", d->owner);
 
