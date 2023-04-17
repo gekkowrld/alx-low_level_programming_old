303950a54f5eb968d0082a85f3d218842878cc62
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-17 15:19:51 +0300

    Create a pointer to the memory of the dog struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+# include "dog.h"
+# include <stdlib.h>
+
+/**
+* init_dog - Points to the memory containing a dog
+* @d: The value to be initialized as a struct
+* @name: The pointer to the name of the dog
+* @age: The pointer to the age of the dog
+* @owner: The pointer to the owner of the dog
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
