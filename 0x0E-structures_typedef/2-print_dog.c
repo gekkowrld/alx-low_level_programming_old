dc3b0cb52e95e277ecca483da5e015e9f3a55fac
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-17 21:55:40 +0300

    How confusing is it

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -19,7 +19,7 @@ void print_dog(struct dog *d)
 	printf("Age: %f\n", d->age);
 
 	if (d->owner == NULL)
-		printf("Name: nil\n");
+		printf("Owner: nil\n");
 	else
 		printf("Owner: %s\n", d->owner);
 
