725452d431044058e277004ac939ae0ea0c7393e
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-19 23:06:18 +0300

    A function that print out a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+# include "function_pointers.h"
+
+/**
+* print_name - Prints the name passed to it
+* @name: The name
+* @f: The callback function
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+		f(name);
+}
