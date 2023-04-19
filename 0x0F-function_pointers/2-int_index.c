30888b7ecddda8301273b58afa73d843528dc029
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-20 00:16:03 +0300

    Add a function that loops through an array and return the first index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+# include "function_pointers.h"
+
+/**
+* int_index - returns the first index of an int
+* @array: The array to be looped over
+* @size: The size of the array to be looped over
+* @cmp: The comparision function
+* Return: -1 or the index number
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i])
+			return (i);
+	}
+
+	return (-1);
+}
