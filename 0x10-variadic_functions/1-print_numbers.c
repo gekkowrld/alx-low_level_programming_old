41d0159dd895d92a95d92dfbba6efd770391af07
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-20 19:47:50 +0300

    Print numbers to the stdout

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+# include "variadic_functions.h"
+
+/**
+* print_numbers - print numbers to the stdout
+* @separator: The seperator character
+* @n: The count of numbers to be printed out
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (separator == NULL)
+		return;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		printf("%s", separator);
+	}
+
+	putchar(10);
+	va_end(args);
+}
