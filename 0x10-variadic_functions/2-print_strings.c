c0813e7a049cf242e21b5deb087023af620a80f6
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-21 00:39:37 +0300

    I agree, no limit is man

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -12,18 +12,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 	char *s;
 
-	if (separator == NULL)
-		return;
+/*	if (separator == NULL) */
+/*		return;*/
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(args, char*);
-		if (s == NULL)
-			printf("nil");
-		printf("%s", s);
-		if (i < n - 1)
+		if (s)
+			printf("%s", s);
+		else
+			printf("(nil)");
+		if (separator && i != n - 1)
 			printf("%s", separator);
 	}
 
