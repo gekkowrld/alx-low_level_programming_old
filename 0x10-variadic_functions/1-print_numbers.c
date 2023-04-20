4f45ec66a815cee8b1566bdff21716344aa437f2
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-21 00:35:18 +0300

    What am I doing?

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,8 +11,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (separator == NULL)
-		return;
+/*	if (separator == NULL) */
+/*		return;*/
 
 	va_start(args, n);
 
