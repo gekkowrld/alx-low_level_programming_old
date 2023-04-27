8cbf2fd3b221002ce8a2486ff4e94642c45fa64b
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-27 17:31:33 +0300

    Add list to head

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,12 @@
+# include "lists.h"
+
+list_t *add_node(list_t ** head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	if(new_node == NULL)
+		return NULL;
+	new_node->str = strdup(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 # define LISTS_H_
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 
 /**
  * struct list_s - singly linked list
@@ -20,5 +22,6 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 # endif
