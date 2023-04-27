49340600a414892c4251a1666ebdae44e5405a0e
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-27 23:05:34 +0300

    Correct the node addition function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,12 +9,20 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = malloc(sizeof(list_t));
+	list_t *new_node;
+	size_t new_str;
 
+	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->str = strdup(str);
+
+	new_str = strlen(str);
+	new_node->len = new_str;
 	new_node->next = *head;
 	*head = new_node;
-	return (new_node);
+
+	return (*head);
 }
+
