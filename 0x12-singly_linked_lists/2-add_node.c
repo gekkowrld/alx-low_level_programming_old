06b1a68d89ff2027778e9d98059613c0d4979a95
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-27 18:03:09 +0300

    Revert the changes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,21 +9,11 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	if (str == NULL)
-		return (NULL);
-
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
-	char *new_str = strdup(str);
-
-	if (new_str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->str = new_str;
+	new_node->str = strdup(str);
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
