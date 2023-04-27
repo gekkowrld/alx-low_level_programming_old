3ac307585cb58e54de46ee29d486e5da14123e63
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-27 18:00:35 +0300

    Rewrite the add_node function

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,19 +1,29 @@
 # include "lists.h"
 
 /**
-* add_node - Adds a node to the beggining
-* @head: The beggining of the list
+* add_node - Adds a node to the beginning
+* @head: The beginning of the list
 * @str: The string
 * Return: new_node or NULL
 */
 
 list_t *add_node(list_t **head, const char *str)
 {
+	if (str == NULL)
+		return (NULL);
+
 	list_t *new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
-	new_node->str = strdup(str);
+	char *new_str = strdup(str);
+
+	if (new_str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->str = new_str;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
