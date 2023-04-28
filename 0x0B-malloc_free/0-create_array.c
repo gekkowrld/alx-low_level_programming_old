340820b8fd70f62e642b953429fde28bce59fa02
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-28 11:54:52 +0300

    Add function that creates an array using malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,74 +1,30 @@
-# include <stdio.h>
-# include <stdlib.h>
+# include "main.h"
 
 /**
-* create_array - creates space for an array
-* @size: The required size of the array
-* @c: The numbers of characters
-* Return: 0 or NULL
+* create_array - Creates arrays using malloc
+* @size: The size of the array
+* @c: The characters to be printed in the array
+* Return: NULL or array
 */
 
-char *create_array(unsigned int size, char c)
+char *create_array(unsigned  int size, char c)
 {
-	int i = 0;
-	char *s;
+	unsigned int i = 0;
+	char *arr;
 
-	if (size == 0 || s == NULL)
+	if (size == 0)
+		return (NULL);
+
+	arr = (char *) malloc(size * sizeof(char));
+
+	if (arr == NULL)
 		return (NULL);
 
 	while (i < size)
 	{
-		s[i] = c;
+		arr[i] = c;
 		i++;
 	}
-	s[size] = '\0';
-	return (s);
-}
 
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *buffer;
-
-    buffer = create_array(98, 'H');
-    if  (buffer == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    simple_print_buffer(buffer, 98);
-    free(buffer);
-    return (0);
+	return (arr);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 # ifndef _MAIN_H_
 # define _MAIN_H_
 
+# include <stdlib.h>
+
 char *create_array(unsigned int size, char c);
 
 #endif
