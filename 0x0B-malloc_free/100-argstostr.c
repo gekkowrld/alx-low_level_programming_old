55244a0826d0fe550a6f48958269356e16507b58
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-04-28 13:54:18 +0300

    Add a function that turns arguments into string

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * argstostr - Converts arguments to string
+ * @ac: The arguments count
+ * @av: The arguments
+ * Return: NULL or r
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int t_l, i, p;
+	char *r;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	t_l = 0;
+	for (i = 0; i < ac; i++)
+	{
+		char *arg = av[i];
+
+		while (*arg)
+		{
+			t_l++;
+			arg++;
+		}
+		t_l++;
+	}
+
+	r = (char *) malloc(t_l * sizeof(char));
+	if (r == NULL)
+		return (NULL);
+
+	p = 0;
+	for (i = 0; i < ac; i++)
+	{
+		char *arg = av[i];
+
+		while (*arg)
+			r[p++] = *arg++;
+
+		r[p++] = '\n';
+	}
+
+	return (r);
+}
